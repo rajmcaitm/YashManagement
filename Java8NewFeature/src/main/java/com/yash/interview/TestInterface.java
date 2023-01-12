c956d4d5943f156f2c4e99bcf10a24a927b1e1f0
rajmcaitm <rajmca.itm@gmail.com>
2023-01-12 22:12:52 +0530

    Update TestInterface.java

--- Java8NewFeature/src/main/java/com/yash/interview/TestInterface.java
+++ Java8NewFeature/src/main/java/com/yash/interview/TestInterface.java
@@ -18,6 +18,8 @@ public class TestInterface implements A, B {
 	
 	
 	public static void main(String[] args) {
+		A a  = new A();
+		B b = new B();
 		TestInterface test = new TestInterface();
 		test.display();//interface A method will called
 		
